0794877982217d88cf8e5c16cb3a2744380919bc
Muchang Bahng <bahngmc@gmail.com>
2020-06-16 16:26:30 +0900

    Actual progress

---
+++ FashionMedia/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FashionMedia
+//
+//  Created by Muchang Bahng on 5/31/20.
+//  Copyright © 2020 Muchang Bahng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
